7005ae1d4456db0c2f95dac2aec27e19c3dbaf81
benok511 <benokeeffe76@gmail.com>
2026-01-05 23:54:39 +0000

    Decided to used flask_restful as it is more robust

--- api_routes.py
+++ api_routes.py
@@ -2,14 +2,27 @@ from flask import Blueprint, jsonify, request, g
 from database import get_db
 from functools import wraps
 from datetime import datetime
+from flask_restful import Api,Resource
 
 api = Blueprint('api',__name__,url_prefix='/api')
 
+myapi = Api(api)
 
-@api.route('/menu',methods = ['GET'])
-def menu():
-    db = get_db()
-    menu = db.execute('Select * from menu')
-    menu = [dict(item) for item in menu ]
+class Menu(Resource):
+    def get(self):
+        db = get_db()
+        menu = db.execute('SELECT * FROM menu').fetchall()
+        menu = [dict(item) for item in menu]
+        return menu  
 
-    return jsonify(menu)
+myapi.add_resource(Menu, '/menu')
+
+class Users(Resource):
+    def get(self):
+        db = get_db()
+        users = db.execute('select * from customers').fetchall()
+        users = [dict(user) for user in users]
+        return users
+    
+
+myapi.add_resource(Users,'/users')
